bb85275444ebb217750267062327a688e72ab244
tetianapavlova <pavlova0409@gmail.com>
2025-12-16 21:33:24 -0500

    feat: add todo form with events and useRef

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,11 +1,23 @@
-function TodoForm() {
+import { useRef } from "react"
+
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef(null)
+
+  const handleAddTodo = (event) => {
+    event.preventDefault()
+
+    const todoTitle = event.target.todoTitle.value.trim()
+    if (!todoTitle) return
+
+    onAddTodo(todoTitle)
+    event.target.reset()
+    inputRef.current.focus()
+  }
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>
-        Add Todo
-      </button>
+      <input id="todoTitle" name="todoTitle" ref={inputRef} required />
+      <button type="submit">Add Todo</button>
     </form>
   )
 }
