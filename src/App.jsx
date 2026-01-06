9c1cb3086050932623c987f8115a014c55f2a7f7
tetianapavlova <pavlova0409@gmail.com>
2026-01-06 13:41:59 -0500

    feat: week 6 conditional rendering, controlled form, todo completion

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,24 @@ function App() {
     const newTodo = {
       id: Date.now(),
       title: todoTitle,
+      isCompleted: false,
     }
 
     setTodoList([newTodo, ...todoList])
+  }
+
+  const completeTodo = (id) => {
+    const updatedTodoList = todoList.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: true } : todo
+    )
+    setTodoList(updatedTodoList)
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   )
 }
